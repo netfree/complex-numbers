7f181f5697f1c04a8af8f9d35515755b3f80d743
Andrei Muntean <andreimuntean909@gmail.com>
2018-10-17 16:07:34 +0300

    almost done

---
+++ ComplexNumbers/ComplexNumbers/Complex.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    class Complex
+    {
+        private int _a;
+        private int _b;
+
+        public int A
+        {
+            set
+            {
+                _a = value;
+            }
+
+            get
+            {
+                return _a;
+            }
+        }
+        public int B
+        {
+            set
+            {
+                _b = value;
+            }
+
+            get
+            {
+                return _b;
+            }
+        }
+
+        public Complex(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public Complex()
+        {
+            A = 0;
+            B = 0;
+        }
+
+        public double Modulus()
+        {
+            return Math.Sqrt(A * A + B * B);
+        }
+
+        public static Complex operator+ (Complex x, Complex y)
+        {
+            var xPlusY = new Complex();
+            xPlusY.A = x.A + y.A;
+            xPlusY.B = x.B + y.B;
+            return xPlusY;
+        }
+
+        public static Complex operator -(Complex x, Complex y)
+        {
+            var xPlusY = new Complex();
+            xPlusY.A = x.A - y.A;
+            xPlusY.B = x.B - y.B;
+            return xPlusY;
+        }
+
+        private string Sign(int a)
+        {
+            if (a < 0)
+                return "-";
+            return "+";
+        }
+
+        public override string ToString()
+        {
+            if (B == 0)
+                return A.ToString();
+
+            return this.A.ToString() + Sign(B) + this.B.ToString() + "i";
+        }
+       
+
+    }
+}
---
+++ ComplexNumbers/ComplexNumbers/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComplexNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+
+            try
+            {
+                var x = new Complex(
+                    Convert.ToInt32(textBox1.Text),
+                    Convert.ToInt32(textBox2.Text)
+                );
+
+                var y = new Complex(
+                   Convert.ToInt32(textBox3.Text),
+                   Convert.ToInt32(textBox4.Text)
+                );
+            
+                var sum = new Complex();
+                var zero = new Complex();
+                sum = x + y;
+
+                label1.Text += "x este: " + x.ToString() + "\n";
+                label1.Text += "y este: " + y.ToString() + "\n";
+                label1.Text += "\n";
+                label1.Text += "suma este: " + (x+y).ToString() + "\n";
+                label1.Text += "diferenta este: " + (x - y).ToString() + "\n";
+                label1.Text += "|x| este: " + x.Modulus().ToString() + "\n";
+                label1.Text += "|y| este: " + y.Modulus().ToString() + "\n";
+
+            }
+            catch
+            {
+                MessageBox.Show("There was an error converting the numbers to int.");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
